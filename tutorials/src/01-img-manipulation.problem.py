309963c984503ece2f94bb62e2fd3ceb3fdbb715
Patrick <moser-patrick@web.de>
2024-10-02 09:24:30 +0200

    Einführung

--- tutorials/src/01-img-manipulation.problem.py
+++ tutorials/src/01-img-manipulation.problem.py
@@ -4,10 +4,11 @@
 # Load, resize and rotate an image. And display it to the screen.
 
 # TODO First step is to import the opencv module which is called 'cv2'
-
+import cv2 
 # TODO Check the opencv version
-
+print(cv2.__version__)
 # TODO Load an image with image reading modes using 'imread'
+img = cv2.imread("./tutorials/data/images/chewing_gum_balls10.jpg", cv2.IMREAD_COLOR)
 # cv2.IMREAD_UNCHANGED  - If set, return the loaded image as is (with alpha
 #                         channel, otherwise it gets cropped). Ignore EXIF
 #                         orientation.
@@ -17,9 +18,15 @@
 #                         color image.
 
 # TODO Resize image with 'resize'
+img = cv2.resize(img ,(500,500))
 
 # TODO Rotate image (but keep it rectangular) with 'rotate'
+img = cv2.rotate(img, cv2.ROTATE_180)
 
 # TODO Save image with 'imwrite'
-
+print(cv2.imwrite("image.jpg", img))
 # TODO Show the image with 'imshow'
+win_name = "Fenster"
+cv2.namedWindow(win_name, cv2.WINDOW_AUTOSIZE)
+cv2.imshow(win_name, img)
+cv2.waitKey(0)
--- tutorials/src/02-pixel-manipulation.problem.py
+++ tutorials/src/02-pixel-manipulation.problem.py
@@ -8,23 +8,42 @@ import cv2
 import numpy as np
 
 # TODO Loading images in grey and color
+img_gray = cv2.imread("./tutorials/data/images/chewing_gum_balls10.jpg", cv2.IMREAD_GRAYSCALE)
+img_color = cv2.imread("./tutorials/data/images/chewing_gum_balls10.jpg", cv2.IMREAD_COLOR)
+
+img = img_gray
 
 # TODO Do some print out about the loaded data using type, dtype and shape
+print(type(img))
+print(img.dtype)
+print(img.shape)
 
 # TODO Continue with the grayscale image
 
 # TODO Extract the size or resolution of the image
+if (img.all() == img_gray.all()):
+    height, width = img.shape
+    print(height)
+    print(width)
+else:
+    height, width, color= img.shape
+    print(height)
+    print(width)
 
 # TODO Resize image
+img = cv2.resize(img,(300,200))
 
 # Row and column access, see https://numpy.org/doc/stable/reference/arrays.ndarray.html for general access on ndarrays
 # TODO Print first row
+print(img[0,:])
 
 # TODO Print first column
+print(img[:,0])
 
 # TODO Continue with the color image
 
 # TODO Set an area of the image to black
+img[100:150,20:80] = 0
 
 # TODO Show the image and wait until key pressed
 
@@ -35,5 +54,9 @@ import numpy as np
 # TODO Save image to a file
 
 # TODO Show the image again
+win_name = "Fenster"
+cv2.namedWindow(win_name, cv2.WINDOW_AUTOSIZE)
+cv2.imshow(win_name, img)
+cv2.waitKey(0)
 
 # TODO Show the original image (copy demo)
