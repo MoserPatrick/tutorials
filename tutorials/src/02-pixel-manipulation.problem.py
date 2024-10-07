3f7914a9381fd5f683a841e45efafdd0133d1554
Patrick <moser-patrick@web.de>
2024-10-07 12:02:17 +0200

    7.10 (aufgabe 3)

--- tutorials/src/02-pixel-manipulation.problem.py
+++ tutorials/src/02-pixel-manipulation.problem.py
@@ -35,25 +35,34 @@ img = cv2.resize(img,(300,200))
 
 # Row and column access, see https://numpy.org/doc/stable/reference/arrays.ndarray.html for general access on ndarrays
 # TODO Print first row
-print(img[0,:])
+#print(img[0,:])
 
 
 # TODO Print first column
-print(img[:,0])
+#print(img[:,0])
 
 # TODO Continue with the color image
+img = img_color
+img = cv2.resize(img,(300,200))
 
 # TODO Set an area of the image to black
-img[100:150,20:80] = 0
+#img[100:150,20:80] = 0
 
 # TODO Show the image and wait until key pressed
+win_name = "Fenster"
+cv2.namedWindow(win_name, cv2.WINDOW_AUTOSIZE)
+cv2.imshow(win_name, img)
+cv2.waitKey(0)
 
 # TODO Find all used colors in the image
-
+all_rgb_codes = img.reshape(-1, img.shape[-1])
+unique_rgbs = np.unique(all_rgb_codes, axis=0, return_counts= False)
+print("Those are the Colors: " + str(unique_rgbs))
 # TODO Copy one part of an image into another one
-
+img_filler = img[100:150,20:80]
+img[150:200,20:80] = img_filler
 # TODO Save image to a file
-
+print(cv2.imwrite("image.jpg", img))
 # TODO Show the image again
 win_name = "Fenster"
 cv2.namedWindow(win_name, cv2.WINDOW_AUTOSIZE)
@@ -61,3 +70,4 @@ cv2.imshow(win_name, img)
 cv2.waitKey(0)
 
 # TODO Show the original image (copy demo)
+
--- tutorials/src/03-video-cam.problem.py
+++ tutorials/src/03-video-cam.problem.py
@@ -7,19 +7,47 @@ import numpy as np
 import cv2
 
 # TODO Capture webcam image
+camera = cv2.VideoCapture(0)
 
 # TODO Get camera image parameters from get()
-
+img_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+img_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+fps = int(camera.get(cv2.CAP_PROP_FPS))
 # TODO Create a window for the video
-
+titel = "test"
+cv2.namedWindow(titel, cv2.WINDOW_FREERATIO)
 # TODO Start a loop
-
+key = -1
+while key == -1:
+    
 # TODO (In loop) read a camera frame and check if that was successful
+    (ret, frame) = camera.read()
 
-# TODO (In loop) create four flipped tiles of the image
+        # TODO (In loop) create four flipped tiles of the image
+    #flipped = cv2.flip(frame, 1)
+    frame_copy = frame.copy()
+    ul = frame_copy
+    ur = np.fliplr(frame_copy)
+    li = np.flipud(frame_copy)
+    lr = np.flipud(np.fliplr(frame_copy))
+    frame = np.zeros_like(frame_copy)
+    frame = cv2.resize(frame, (-1,-1),fx=2.0,fy=2.0)
+    frame.[:height,:width] = ul
+    frame.[:height,width:width+2] = ur
+    frame.[height:height+2,:width] = li
+    frame.[height:height+2,width:width+2] = lr
+    
+        
+    
+
 
 # TODO (In loop) display the image
+    cv2.imshow(titel, flipped)
 
 # TODO (In loop) press q to close the window and exit the loop
+    key = cv2.waitKey(int((1/fps)*1000))
 
 # TODO Release the video capture object and window
+print(key)
+camera.release()
+cv2.destroyAllWindows()
